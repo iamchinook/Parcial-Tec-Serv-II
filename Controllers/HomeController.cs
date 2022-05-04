418aca5fd989911596031e0e0cb53392908d625e
Chino <Chino@DESKTOP-F4R0G5L>
2022-05-04 11:52:58 -0300

    Agregar archivos de proyecto.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+
+namespace MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       public ActionResult Index()
+        {
+            //return RedirectToAction("Buscar", "Usuario", routeValues: new { nombre = "Alejandro Garcia" });        
+            return RedirectToAction("MostrarHolaMundo", "Usuario");        
+
+        }
+    }
+}
---
+++ Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace MVC.Controllers
+{
+    public class UsuarioController : Controller
+    {
+       public ActionResult Buscar(string nombre)
+        {
+            string input = "Resultado de la busqueda: " + nombre;
+            return Content(input);
+        }
+        public ActionResult MostrarHolaMundo()
+        {
+            string input = "Hola Mundo";
+            return Content(input);
+        }
+        public ActionResult DarBienvenida(string nombre)
+        {
+            string input = "Bienvenido usuario: " + nombre;
+            return Content(input);
+        }
+    }
+}
